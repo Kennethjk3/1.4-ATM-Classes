9fdcf597c607505dd6b75c678375e409c955b96c
Kenneth Kier <Kennethjk3@gmail.com>
2015-07-05 23:12:24 -0400

    Create functions.rb

---
+++ functions.rb
@@ -0,0 +1,14 @@
+# require './user_accounts.rb'
+require '.user.rb'
+button = ""
+# account = User_account.new("***Something with the CSV")
+
+print "Welcome to the Greatest ATM of ALL TIME!\n \n"
+
+
+while button == "" do
+  puts "How may I assit you today?\n"
+  break
+
+
+end
